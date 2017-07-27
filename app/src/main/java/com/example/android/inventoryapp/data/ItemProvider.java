e4649653b28fb6f0865ad68867543819618a8b11
SzasznikaJanos <szasznikajanos@gmail.com>
2017-07-28 00:03:58 +0300

    Initial commit

---
+++ app/src/main/java/com/example/android/inventoryapp/data/ItemProvider.java
@@ -0,0 +1,266 @@
+package com.example.android.inventoryapp.data;
+
+import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.util.Log;
+
+import com.example.android.inventoryapp.data.ItemContract.ItemEntry;
+
+/**
+ * Created by Szasznika Janos on 25.07.2017.
+ */
+
+public class ItemProvider extends ContentProvider {
+
+    // Tag for log messages
+    public static final String LOG_TAG = ItemProvider.class.getSimpleName();
+    private ItemDbHelper mDbHelper;
+
+    /**
+     * URI matcher code for the content URI for the items table
+     */
+    public static final int ITEMS = 100;
+
+    /**
+     * URI matcher code for the content URI for a single item in the items table
+     */
+    public static final int ITEM_ID = 101;
+
+    /**
+     * URI matcher object to match a context URI to a corresponding code.
+     * The input passed into the constructor represents the code to return for the root URI.
+     * It's common to use NO_MATCH as the input for this case.
+     */
+    private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    // Static initializer. This is run the first time anything is called from this class.
+    static {
+        sUriMatcher.addURI(ItemContract.CONTENT_AUTHORITY, ItemContract.PATH_ITEMS, ITEMS);
+        sUriMatcher.addURI(ItemContract.CONTENT_AUTHORITY, ItemContract.PATH_ITEMS + "/#", ITEM_ID);
+    }
+
+    /**
+     * Initialize the provider and the database helper object.
+     */
+    @Override
+    public boolean onCreate() {// ContentProvider methods.
+        mDbHelper = new ItemDbHelper(getContext());
+        return true;
+    }
+
+    /**
+     * Perform the query for the given URI. Use the given projection, selection, selection arguments, and sort order.
+     */
+    @Override
+    public Cursor query(Uri uri, String[] projection,
+                        String selection, String[] selectionArgs,
+                        String sortOrder) {
+        // Get readable database
+        SQLiteDatabase database = mDbHelper.getReadableDatabase();
+
+        // This  cursor will hold the result of the query
+        Cursor cursor = null;
+
+        // Figure out if the URI matcher can match the URI to a specific code
+        int match = sUriMatcher.match(uri);
+        switch (match) {
+            case ITEMS:
+                cursor = database.query(ItemEntry.TABLE_NAME, projection, selection,
+                        selectionArgs, null, null, sortOrder);
+                break;
+            case ITEM_ID:
+                selection = ItemEntry._ID + "=?";
+                selectionArgs = new String[]{String.valueOf(ContentUris.parseId(uri))};
+
+                // This will perform a query on the items table where the _id equals 3 to return a
+                // Cursor containing that row of the table.
+                cursor = database.query(ItemEntry.TABLE_NAME, projection, selection, selectionArgs,
+                        null, null, sortOrder);
+                break;
+            default:
+                throw new IllegalArgumentException("Cannot query unknown URI " + uri);
+        }
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);
+        return cursor;
+    }
+
+    /**
+     * Insert new data into the provider with the given ContentValues.
+     */
+    @Override
+    public Uri insert(Uri uri, ContentValues contentValues) {
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case ITEMS:
+                return insertItem(uri, contentValues);
+            default:
+                throw new IllegalArgumentException("Insertion is not supported for " + uri);
+        }
+    }
+
+    /**
+     * Insert a item into the database with the given content values. Return the new content URI
+     * for that specific row in the database.
+     */
+    private Uri insertItem(Uri uri, ContentValues values) {
+
+        // Check that the name is not null
+        String name = values.getAsString(ItemEntry.COLUMN_ITEM_NAME);
+        String price = values.getAsString(ItemEntry.COLUMN_ITEM_PRICE);
+        String quantity = values.getAsString(ItemEntry.COLUMN_ITEM_QUANTITY);
+        String image = values.getAsString(ItemEntry.COLUMN_ITEM_IMAGE);
+        String supplier = values.getAsString(ItemEntry.COLUMN_ITEM_SUPPLIER);
+        String email = values.getAsString(ItemEntry.COLUMN_SUPPLIER_EMAIL);
+
+        if (price == null) {
+            throw new IllegalArgumentException("Item requires a price");
+        }
+        if (quantity == null || Integer.parseInt(quantity) < 0) {
+            throw new IllegalArgumentException("Item requires a quantity");
+        }
+        if (name == null) {
+            throw new IllegalArgumentException("Item requires a name");
+        }
+        if (image == null) {
+            throw new IllegalArgumentException("Item requires a image");
+        }
+        if (supplier == null) {
+            throw new IllegalArgumentException("Item requires a supplier");
+        }
+        if (email == null) {
+            throw new IllegalArgumentException("Item requires a email");
+        }
+
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+        long id = db.insert(ItemEntry.TABLE_NAME, null, values);
+
+        if (id == 1) {
+            Log.e(LOG_TAG, "Failed to insert row for " + uri);
+            return null;
+        } else if (id != 0)
+            getContext().getContentResolver().notifyChange(uri, null);
+        // Once we know the ID of the new row in the table,
+        // return the new URI with the ID appended to the end of it
+        return ContentUris.withAppendedId(uri, id);
+    }
+
+    /**
+     * Updates the data at the given selection and selection arguments, with the new ContentValues.
+     */
+    @Override
+    public int update(Uri uri, ContentValues contentValues, String selection,
+                      String[] selectionArgs) {
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case ITEMS:
+                return updateItem(uri, contentValues, selection, selectionArgs);
+            case ITEM_ID:
+                // For the ITEM_ID code, extract out the ID from the URI,
+                // so we know which row to update. Selection will be "_id=?" and selection
+                // arguments will be a String array containing the actual ID.
+                selection = ItemEntry._ID + "=?";
+                selectionArgs = new String[]{String.valueOf(ContentUris.parseId(uri))};
+                int rowsUpdated = updateItem(uri, contentValues, selection, selectionArgs);
+                if (rowsUpdated != 0)
+                    getContext().getContentResolver().notifyChange(uri, null);
+                return rowsUpdated;
+            default:
+                throw new IllegalArgumentException("Update is not supported for " + uri);
+        }
+    }
+
+    /**
+     * Update items in the database with the given content values. Apply the changes to the rows
+     * specified in the selection and selection arguments (which could be 0 or 1 or more items).
+     * Return the number of rows that were successfully updated.
+     */
+    private int updateItem(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        // If the {@link ItemEntry#COLUMN_ITEM_NAME} key is present,
+        // check that the name value is not null.
+        if (values.containsKey(ItemEntry.COLUMN_ITEM_NAME)) {
+            String name = values.getAsString(ItemEntry.COLUMN_ITEM_NAME);
+            if (name == null) {
+                throw new IllegalArgumentException("Item requires a name");
+            }
+        }
+
+        // If the {@link ItemEntry#COLUMNT_ITEM_QUANTITY} key is present,
+        // check that the gender value is valid.
+        if (values.containsKey(ItemEntry.COLUMN_ITEM_QUANTITY)) {
+            Integer quantity = values.getAsInteger(ItemEntry.COLUMN_ITEM_QUANTITY);
+            if (quantity == null) {
+                throw new IllegalArgumentException("Item requires valid quantity");
+            }
+        }
+
+        // If the {@link ItemEntry#COLUMN_ITEM_PRICE} key is present,
+        // check that the weight value is valid.
+        if (values.containsKey(ItemEntry.COLUMN_ITEM_PRICE)) {
+            // Check that the weight is greater than or equal to 0 kg
+            Integer price = values.getAsInteger(ItemEntry.COLUMN_ITEM_PRICE);
+            if (price != null && price < 0) {
+                throw new IllegalArgumentException("Item requires valid price");
+            }
+        }
+        // If there are no values to update, then don't try to update the database
+        if (values.size() == 0) {
+            return 0;
+        }
+        // Otherwise, get writable database to update the data
+        SQLiteDatabase database = mDbHelper.getWritableDatabase();
+
+        // Returns the number of database rows affected by the update statement
+        return database.update(ItemEntry.TABLE_NAME, values, selection, selectionArgs);
+    }
+
+    /**
+     * Delete the data at the given selection and selection arguments.
+     */
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        // Get writeable database
+        SQLiteDatabase database = mDbHelper.getWritableDatabase();
+
+        final int match = sUriMatcher.match(uri);
+        int rowsDeleted;
+        switch (match) {
+            case ITEMS:
+                // Delete all rows that match the selection and selection args
+                rowsDeleted = database.delete(ItemEntry.TABLE_NAME, selection, selectionArgs);
+                if (rowsDeleted != 0)
+                    getContext().getContentResolver().notifyChange(uri, null);
+                return rowsDeleted;
+            case ITEM_ID:
+                // Delete a single row given by the ID in the URI
+                selection = ItemEntry._ID + "=?";
+                selectionArgs = new String[]{String.valueOf(ContentUris.parseId(uri))};
+                rowsDeleted = database.delete(ItemEntry.TABLE_NAME, selection, selectionArgs);
+                if (rowsDeleted != 0)
+                    getContext().getContentResolver().notifyChange(uri, null);
+                return rowsDeleted;
+            default:
+                throw new IllegalArgumentException("Deletion is not supported for " + uri);
+        }
+    }
+
+    /**
+     * Returns the MIME type of data for the content URI.
+     */
+    @Override
+    public String getType(Uri uri) {
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case ITEMS:
+                return ItemEntry.CONTENT_LIST_TYPE;
+            case ITEM_ID:
+                return ItemEntry.CONTENT_ITEM_TYPE;
+            default:
+                throw new IllegalStateException("Unknown URI " + uri + " with match " + match);
+        }
+    }
+}
